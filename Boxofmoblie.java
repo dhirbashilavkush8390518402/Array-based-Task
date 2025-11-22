27621920f5b9bd79c86209c0bada44ba670800be
dhirbashilavkush8390518402 <s24_dhirbashi_lavkush@mgmcen.ac.in>
2025-11-22 12:16:43 +0530

    inheritance application case study
    inheritance application case study

---
+++ Boxofmoblie.java
@@ -0,0 +1,16 @@
+public class Boxofmobile
+{
+ public static void main(String []args)
+  {
+   Moblie M = new Moblie("Samsung","M35",31000);
+   M.Display();
+   M.setmodelname("M25");
+   M.getmodelname();
+ 
+   M.setprice(280000);
+   M.getprice();
+  
+   M.Display();
+  }
+
+} 
